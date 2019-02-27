dc1f8aff3ee75221c8730b3a5b25e1ae73e2704f
SashaMikhailau <azyainhome@gmail.com>
2019-02-28 00:05:58 +0300

    hw03init

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,37 +1,91 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.Util;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+    private final static RowMapper<Meal> MEALMAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    @Autowired
+    public JdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        this.simpleJdbcInsert =
+                new SimpleJdbcInsert(jdbcTemplate)
+                        .withTableName("meals")
+                        .usingGeneratedKeyColumns("id");
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("user_id", userId);
+        mapSqlParameterSource.addValue("datetime", meal.getDateTime());
+        mapSqlParameterSource.addValue("description", meal.getDescription());
+        mapSqlParameterSource.addValue("calories", meal.getCalories());
+
+        if (meal.isNew()) {
+            Integer key = (Integer) simpleJdbcInsert.executeAndReturnKey(mapSqlParameterSource);
+            meal.setId(key);
+        } else {
+            mapSqlParameterSource.addValue("id", meal.getId());
+            int i = namedParameterJdbcTemplate.update("UPDATE meals SET datetime=:datetime, " +
+                    "calories =:calories, description =:description WHERE id =:id AND " +
+                    "user_id =:user_id", mapSqlParameterSource);
+            if (i <= 0) {
+                return null;
+            }
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+       return jdbcTemplate.update("DELETE FROM meals WHERE id = ? AND user_id = ?", id, userId)!=0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM meals WHERE id = ? AND user_id = ?",MEALMAPPER
+                ,id,userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return getAllFiltered(userId, meal -> true);
     }
 
     @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId){
+        return getAllFiltered(userId, meal -> Util.isBetween(meal.getDateTime(), startDate, endDate));
+    }
+
+    private List<Meal> getAllFiltered(int userId, Predicate<Meal> filter) {
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ?", MEALMAPPER,userId)
+                .stream()
+                        .filter(filter)
+                        .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                        .collect(Collectors.toList());
     }
 }
